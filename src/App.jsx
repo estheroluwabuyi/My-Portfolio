030ce96943487570a68ad063dbd511b8634f51fe
Esther Okiki <estherokiki15@gmail.com>
2025-05-14 22:52:47 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-5 text-3xl font-bold underline">
+      Hello, Tailwind CSS is working!
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Preloader
