2cf2a11a40ce4e26f90f7569894979c0d95d8f79
Esther Okiki <estherokiki15@gmail.com>
2025-06-01 00:20:04 +0100

    feat: add aminations to Tools.jsx and AboutMe.jsx page

--- src/components/sections/Tools.jsx
+++ src/components/sections/Tools.jsx
@@ -68,7 +68,6 @@ const toolsList = [
 
 function Tools() {
   const [active, setActive] = useState(null);
-
   
 
   return (
@@ -99,9 +98,9 @@ function Tools() {
                 src={tool.img}
                 alt={tool.title}
                 className={clsx(
-                  "w-16 h-16 mb-2 transition duration-600 ease",
+                 "w-16 h-16 mb-2 transition-transform duration-300 ease-in-out transform-gpu touch-transform",
                   index === 4 && "bg-primary rounded-[100%] border",
-                active === index && "scale-95 rotate-[360deg]"
+                  active === index && "-scale-[1] rotate-180"
                 )}
               />
               {/* scale(-1) rotate(180deg) */}
